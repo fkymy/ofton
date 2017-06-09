530d3c1e4b9641ddee6aa9117a0507997e17df3b
Yuske Fukuyama <yuskefuku@gmail.com>
2017-06-10 03:13:00 +0900

    add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :entry, counter_cache: true
+  validates :entry_id, presence: true
+  validates :body, presence: true
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,4 @@
 class Entry < ApplicationRecord
   has_many :comments, dependent: :destroy
+  validates :body, presence: true
 end
