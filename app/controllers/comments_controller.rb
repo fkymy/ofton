e2cbde1654dec57d193e5fa3162ca7b35cfe0eca
Yuske Fukuyama <yuskefuku@gmail.com>
2017-06-17 18:58:54 +0900

    edit controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,21 @@
 class CommentsController < ApplicationController
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to @comment
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :author, :body)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,34 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
