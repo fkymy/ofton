7d1c5d186aabd7d419159ec5587e6627b925cc14
Yuske Fukuyama <yuskefuku@gmail.com>
2017-06-19 19:42:50 +0900

    test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create(author: 'yushi', body: 'test', comments_count: 0)
+Post.create(author: 'yushi', body: 'test', comments_count: 0)
+Post.create(author: 'yushi', body: 'test', comments_count: 0)
+Post.create(author: 'yushi', body: 'test', comments_count: 0)
+
+Comment.create(post_id: 1, author: 'yushi', body: 'test comment')
+Comment.create(post_id: 1, author: 'yushi', body: 'test comment')
+Comment.create(post_id: 1, author: 'yushi', body: 'test comment')
+Comment.create(post_id: 2, author: 'yushi', body: 'test comment')
+Comment.create(post_id: 2, author: 'yushi', body: 'test comment')
+Comment.create(post_id: 3, author: 'yushi', body: 'test comment')
