cabcfa32ac2a9f7296a88872bff6a1e423db9a5d
Yuske Fukuyama <yuskefuku@gmail.com>
2017-06-23 16:03:42 +0900

    fix buggers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts do
-    resources :comments
+    resources :comments, only: [:new, :create]
   end
 end
