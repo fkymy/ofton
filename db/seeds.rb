7b1f2e6b10148e08cd7d3d7b9d3cdcc2441f94f7
Yuske Fukuyama <yuskefuku@gmail.com>
2017-06-24 18:20:37 +0900

    tmp seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Post.create(author: 'yushi', body: 'test', comments_count: 0)
-Post.create(author: 'yushi', body: 'test', comments_count: 0)
-Post.create(author: 'yushi', body: 'test', comments_count: 0)
-Post.create(author: 'yushi', body: 'test', comments_count: 0)
+
+body1 = "消えたいです\n生きてるのが辛いです\n何にも楽しくないです\nどうしたら前みたいに生きられるのかな？"
+body2 = "寝れない状態が、続いてます。\n\n同じ感じの人いませんか。"
+body3 = "恥の多い生涯を送って来ました。\n自分には、人間の生活というものが、見当つかないのです。自分は東北の田舎に生れましたので、汽車をはじめて見たのは、よほど大きくなってからでした。自分は停車場のブリッジを、上って、降りて、そうしてそれが線路をまたぎ越えるために造られたものだという事には全然気づかず、ただそれは停車場の構内を外国の遊戯場みたいに、複雑に楽しく、ハイカラにするためにのみ、設備せられてあるものだとばかり思っていました。しかも、かなり永い間そう思っていたのです。ブリッジの上ったり降りたりは、自分にはむしろ、ずいぶん垢抜あかぬけのした遊戯で、それは鉄道のサーヴィスの中でも、最も気のきいたサーヴィスの一つだと思っていたのですが、のちにそれはただ旅客が線路をまたぎ越えるための頗る実利的な階段に過ぎないのを発見して、にわかに興が覚めました。\nどうにもできない情慾の種子を植えつけられた許ばかりに\n善だ悪だ罪だ罰だと呪のろわるるばかり\nどうにもできない只まごつくばかり\n抑え摧くだく力も意志も授けられぬ許りに"
+
+Post.create(author: 'yushi', body: body1, comments_count: 3)
+Post.create(author: 'yushi', body: body2, comments_count: 2)
+Post.create(author: 'yushi', body: body3, comments_count: 1)
+Post.create(author: 'yushi', body: body3, comments_count: 0)
+Post.create(author: 'yushi', body: body1, comments_count: 3)
+Post.create(author: 'yushi', body: body2, comments_count: 2)
+Post.create(author: 'yushi', body: body3, comments_count: 1)
+Post.create(author: 'yushi', body: body1, comments_count: 3)
+Post.create(author: 'yushi', body: body2, comments_count: 2)
+Post.create(author: 'yushi', body: body3, comments_count: 1)
+Post.create(author: 'yushi', body: body3, comments_count: 0)
+Post.create(author: 'yushi', body: body3, comments_count: 0)
 
 Comment.create(post_id: 1, author: 'yushi', body: 'test comment')
 Comment.create(post_id: 1, author: 'yushi', body: 'test comment')
