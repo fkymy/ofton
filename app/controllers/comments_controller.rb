c3085cf22d5c3a9240617b87498d96443fe91ffb
Yuske Fukuyama <yuskefuku@gmail.com>
2017-06-26 21:42:00 +0900

    validation msgs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to post_path(@post)
     else
-      render 'new'
+      redirect_to post_path(@post)
     end
   end
 
