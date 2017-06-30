28c2781a385ac8b624e97625b50418a69b56a9cf
Yuske Fukuyama <yuskefuku@gmail.com>
2017-07-01 01:20:22 +0900

    add static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def about
+  end
+
+  def policy
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
+  # static pages
+  get 'about' => 'static_pages#about'
+  get 'policy' => 'static_pages#policy'
+
   concern :paginatable do
     get '(page/:page)', action: :index, on: :collection, as: ''
   end
