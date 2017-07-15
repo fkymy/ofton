5a30caa74009b29acb1ba587317acf3f4ca29d0c
Yuske Fukuyama <yuskefuku@gmail.com>
2017-07-15 19:31:06 +0900

    display datetime

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -8,4 +8,12 @@ class CommentDecorator < ApplicationDecorator
   def avatar
     "https://api.adorable.io/avatars/75/#{object.id}"
   end
+
+  def datetime
+    if object.created_at > 1.week.ago
+      "#{h.time_ago_in_words(object.created_at)}前"
+    else
+      l(object.created_at, format: :long)
+    end
+  end
 end
