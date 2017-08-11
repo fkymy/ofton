f5c5886fa6dd3486a30bfad4fc1c62308f3b8fc4
Yuske Fukuyama <yuskefuku@gmail.com>
2017-08-11 19:09:29 +0900

    change default

--- db/migrate/20170811092138_add_level_to_posts_and_comments.rb
+++ db/migrate/20170811092138_add_level_to_posts_and_comments.rb
@@ -1,6 +1,6 @@
 class AddLevelToPostsAndComments < ActiveRecord::Migration[5.1]
   def change
-    add_column :posts, :generated_by, :integer, null: false, default: 0, after: :comments_count
-    add_column :comments, :generated_by, :integer, null: false, default: 0, after: :body
+    add_column :posts, :generated_by, :integer, null: false, default: 1, after: :comments_count
+    add_column :comments, :generated_by, :integer, null: false, default: 1, after: :body
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170811092138) do
     t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "generated_by", default: 0, null: false
+    t.integer "generated_by", default: 1, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170811092138) do
     t.integer "comments_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "generated_by", default: 0, null: false
+    t.integer "generated_by", default: 1, null: false
   end
 
 end
