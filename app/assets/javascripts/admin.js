77e42681850fef4d4c1379c4e190e43ed1f23423
Yuske Fukuyama <yuskefuku@gmail.com>
2017-08-13 19:43:11 +0900

    admin assets

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,16 @@
+
+// this is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// any javascript/coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// it's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. javascript code in this file should be added after the last require_* statement.
+//
+// read sprockets readme (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
