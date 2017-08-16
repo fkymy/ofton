0d30ce39c51d30c06fb784d0cf90fce5a4cb55e2
Yuske Fukuyama <yuskefuku@gmail.com>
2017-08-16 21:47:40 +0900

    comment out admin/resources

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,25 +1,25 @@
 class Admin::CommentsController < ApplicationController
-  # decorates_assigned :comment
-
-  def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-
-    @comment.author = helpers.simple_nkf(comment_params[:author])
-    @comment.body = helpers.simple_nkf(comment_params[:body])
-
-    if @comment.save && Post.increment_counter(:comments_count, params[:post_id])
-      Slack::CommentCreatedNotifier.notify(Slack::Template::CommentCreatedMessage.format(@comment))
-
-      redirect_to post_path(@post)
-    else
-      redirect_to post_path(@post)
-    end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:post_id, :author, :body)
-  end
+  # # decorates_assigned :comment
+  #
+  # def create
+  #   @post = Post.find(params[:post_id])
+  #   @comment = @post.comments.create(comment_params)
+  #
+  #   @comment.author = helpers.simple_nkf(comment_params[:author])
+  #   @comment.body = helpers.simple_nkf(comment_params[:body])
+  #
+  #   if @comment.save && Post.increment_counter(:comments_count, params[:post_id])
+  #     Slack::CommentCreatedNotifier.notify(Slack::Template::CommentCreatedMessage.format(@comment))
+  #
+  #     redirect_to post_path(@post)
+  #   else
+  #     redirect_to post_path(@post)
+  #   end
+  # end
+  #
+  # private
+  #
+  # def comment_params
+  #   params.require(:comment).permit(:post_id, :author, :body)
+  # end
 end
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,35 +1,35 @@
 class Admin::PostsController < ApplicationController
-
-  def index
-    @posts = Post.all.order_by_default.page(params[:page])
-  end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-
-    @post.author = helpers.simple_nkf(post_params[:author])
-    @post.body = helpers.simple_nkf(post_params[:body])
-
-    if @post.save
-      Slack::PostCreatedNotifier.notify(Slack::Template::PostCreatedMessage.format(@post))
-
-      redirect_to @post
-    else
-      render 'new'
-    end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:author, :body)
-  end
+  #
+  # def index
+  #   @posts = Post.all.order_by_default.page(params[:page])
+  # end
+  #
+  # def show
+  #   @post = Post.find(params[:id])
+  # end
+  #
+  # def new
+  #   @post = Post.new
+  # end
+  #
+  # def create
+  #   @post = Post.new(post_params)
+  #
+  #   @post.author = helpers.simple_nkf(post_params[:author])
+  #   @post.body = helpers.simple_nkf(post_params[:body])
+  #
+  #   if @post.save
+  #     Slack::PostCreatedNotifier.notify(Slack::Template::PostCreatedMessage.format(@post))
+  #
+  #     redirect_to @post
+  #   else
+  #     render 'new'
+  #   end
+  # end
+  #
+  # private
+  #
+  # def post_params
+  #   params.require(:post).permit(:author, :body)
+  # end
 end
