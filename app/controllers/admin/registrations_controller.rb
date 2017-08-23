d9096010f4533fdb7a0776dc9f0366f619d9af22
Yuske Fukuyama <yuskefuku@gmail.com>
2017-08-23 10:59:00 +0900

    change devise custom scopes

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Admin::Devise::RegistrationsController < Devise::RegistrationsController
+class Admin::RegistrationsController < Devise::RegistrationsController
   layout 'admin'
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
