7ee0a8b26a1ade4d4cc9fbbb4f59ac0c90ee5af8
Yuske Fukuyama <yuskefuku@gmail.com>
2017-09-06 14:14:00 +0900

    add date to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,9 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @today = Time.now.strftime("%F")
+    @month_ago = 30.days.ago.strftime("%F")
+    @week_ago = 7.days.ago.strftime("%F")
     @data = KpiManager.fetch_data
   end
 end
