a9b6e981dd2f6603ecddacf7ad587fb46464d17a
Yuske Fukuyama <yuskefuku@gmail.com>
2017-10-14 19:49:44 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root 'dashboard#index'
-    post '/metrics', to: 'dashboard#save_metrics'
+    post 'metrics', to: 'dashboard#save_metrics'
 
     resources :admins, only: [:index]
     resources :posts, only: [:index, :show]
